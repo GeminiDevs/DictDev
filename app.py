419a644d1bfee1edbc1f91d3539badca5adb3f95
OtienoSamwel <otienosamwel135@gmail.com>
2020-11-19 17:51:27 +0300

    connected api to frontend

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import dictionary
+from dictionary import dictionary
 
 app = Flask(__name__)
 
@@ -7,6 +9,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+# get the form input
+@app.route('/getform', methods=['POST','GET'])
+def getform():
+    if request.method == "POST":
+        search = request.form['search']
+       
+    return render_template("index.html",  meaning=dictionary(search), word=search.upper()) 
 
 if __name__ == '__main__':
     app.run(debug=True)
